61834e46caf64978d0a4df1d75297ff7cefd94f0
Javier Sebastian <u20211e429@upc.edu.pe>
2023-11-14 22:45:31 -0500

    feat: cors config

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.product import product
 from routes.user import user
 from routes.comments import comments
@@ -9,6 +10,15 @@ app = FastAPI(
     title="Nissou API",
     description="Simple API made with FastAPI and MongoDB",
     version="1.0.0"
+)
+
+# Configuración para permitir todas las solicitudes CORS (debes ajustar según tus necesidades)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Puedes especificar los dominios permitidos en lugar de "*"
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 @app.get("/", include_in_schema=False)
